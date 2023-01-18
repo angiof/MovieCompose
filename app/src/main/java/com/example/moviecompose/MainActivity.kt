338d7b26a81f48c0e19350971d82eaa7412b25be
angelo ferretti <31105678+angiof@users.noreply.github.com>
2023-01-18 14:52:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/moviecompose/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.moviecompose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.moviecompose.dto.FakePeliculas
+import com.example.moviecompose.dto.Pelicula
+import com.example.moviecompose.ui.theme.MovieComposeTheme
+import java.util.Objects
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    //SimpleLista(pelicula = FakePeliculas.getAllPeliculas())
+                    Recy(pelicula = FakePeliculas.getAllPeliculas())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SimpleLista(pelicula: List<Pelicula>) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        pelicula.forEach {
+            //Text(text = it.nome)
+            ListaMoreBella(title = it.nome)
+            Text(text = it.descrizione)
+        }
+
+    }
+
+}
+
+@Composable
+fun ListaMoreBella(title: String) {
+    Text(
+        text = title,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.comom_padding_defoult)),
+        style = MaterialTheme.typography.h6
+    )
+
+}
+
+@Composable
+fun ItemsListClik(title: String, modifier: Modifier = Modifier) {
+    Text(
+        text = title,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.comom_padding_defoult)),
+        style = MaterialTheme.typography.h6
+    )
+
+}
+
+@Composable
+fun Recy(pelicula: List<Pelicula>) {
+    val context = LocalContext.current
+    LazyColumn(content = {
+        items(pelicula.size) {
+            val pelicula = pelicula[it]
+            Divider()
+            ListItemsRecy(pelicula = pelicula)
+        }
+    })
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ListItemsRecy(pelicula: Pelicula, modifier: Modifier = Modifier) {
+    ListItem(
+        text = { Text(text = pelicula.nome, style = MaterialTheme.typography.h6) },
+        secondaryText = {
+            Text(
+                text = pelicula.descrizione,
+                style = MaterialTheme.typography.subtitle1
+            )
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MovieComposeTheme {
+        SimpleLista(pelicula = FakePeliculas.getAllPeliculas())
+    }
+}
---
+++ app/src/main/java/com/example/moviecompose/dto/Pelicula.kt
@@ -0,0 +1,8 @@
+package com.example.moviecompose.dto
+
+data class Pelicula(
+    val nome: String,
+    val descrizione: String,
+    val punteggio: String,
+    val urlPhoto: String
+)
